6a322912f9c5227d30d8fa69037e576dc8724c87
Naimish Shukla <contactwithn2s@gmail.com>
2023-04-24 10:44:07 +0530

    Add files via upload
    added website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+
+
+  theme: {
+    extend: {
+
+      
+        
+
+    },
+  },
+  // plugins: [],
+
+  plugins: [require("tw-elements/dist/plugin")]
+
+}
